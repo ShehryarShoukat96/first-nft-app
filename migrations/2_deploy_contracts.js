2273d9b6ba147f7e630418671e661d91430530b0
Shehryar <14bscssshoukat@seecs.edu.pk>
2021-06-02 02:55:33 +0500

    add Auction deployement in 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+const SimpleStorage = artifacts.require("./SimpleStorage.sol");
+const Auction = artifacts.require("./Auction.sol");
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network, accounts) {
   deployer.deploy(SimpleStorage);
+  deployer.deploy(Auction, "10", accounts[0], "SKB", "GPU");
 };
