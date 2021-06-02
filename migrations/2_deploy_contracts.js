3276ac5589b5fd6b5e3af374626b177e72bd3828
Shehryar <14bscssshoukat@seecs.edu.pk>
2021-06-03 03:39:21 +0500

    update 2_deploy_contracts.js Auction deployement

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,5 +3,12 @@ const Auction = artifacts.require("./Auction.sol");
 
 module.exports = function(deployer, network, accounts) {
   deployer.deploy(SimpleStorage);
-  deployer.deploy(Auction, "10", accounts[0], "SKB", "GPU");
+  deployer.deploy(Auction,
+    "10", // uint _biddingTime,
+    accounts[0], // address _owner
+    "1", // uint _tokenId
+    "https://ipfs.io/ipfs/QmaSED9ZSbdGts5UZqueFJjrJ4oHH3GnmGJdSDrkzpYqRS?filename=the-chainlink-knight.json", // string _tokenURI
+    "LOCO", // string _tokenName
+    "LC" // string _tokenSymbol
+  );
 };
