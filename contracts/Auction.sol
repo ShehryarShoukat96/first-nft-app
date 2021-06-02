ae7ddff31c7a33753798e4c71a99fbe86367ef3a
Shehryar <14bscssshoukat@seecs.edu.pk>
2021-06-03 03:39:02 +0500

    add ERC721 implementation in Auction.sol

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -1,45 +1,54 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.6;
 
-import "./SafeMath.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-// Inspired by https://www.myhsts.org/blog/ethereum-dapp-with-evm-remix-golang-truffle-and-solidity-part1.html
-contract Auction {
+// Inspired by:
+// https://www.myhsts.org/blog/ethereum-dapp-with-evm-remix-golang-truffle-and-solidity-part1.html
+// https://betterprogramming.pub/how-to-create-nfts-with-solidity-4fa1398eb70a
+contract Auction is ERC721 {
     using SafeMath for uint256;
 
+    event NftCreated(uint indexed tokenId);
     event BidEvent(address indexed highestBidder, uint256 highestBid);
     event WithdrawalEvent(address withdrawer, uint256 amount);
     event CanceledEvent(string message, uint256 time);
 
     uint256 public constant auctionTime = 10 minutes;
+
     uint256 public auctionStart;
     uint256 public auctionEnd;
     uint256 public highestBid;
     address internal auctionOwner;
     address public highestBidder;
 
+
     enum AuctionState { STARTED, CANCELLED }
 
-    struct Car {
-        string Brand;
-        string Rnumber;
-    }
-
-    Car public Mycar;
+    uint256 public tokenId;
     address[] bidders;
     mapping(address => uint) public bids;
     AuctionState public STATE;
 
-    constructor (uint _biddingTime, address  _owner,string memory _brand, string memory _Rnumber) {
+    constructor (
+        uint _biddingTime,
+        address _owner,
+        uint _tokenId,
+        string memory _tokenURI,
+        string memory _tokenName,
+        string memory _tokenSymbol
+    ) ERC721 (_tokenName, _tokenSymbol) {
         auctionOwner = _owner;
         auctionStart = block.timestamp;
         auctionEnd = auctionStart.add(_biddingTime.mul(auctionTime));
         STATE = AuctionState.STARTED;
+        tokenId = _tokenId;
 
-        Mycar = Car({
-            Brand: _brand,
-            Rnumber: _Rnumber
-        });
+        _safeMint(_owner, _tokenId);
+        _setTokenURI(_tokenId, _tokenURI);
+
+        emit NftCreated(_tokenId);
     }
 
     modifier anOngoingAuction() {
@@ -58,6 +67,7 @@ contract Auction {
 
     function bid() public payable anOngoingAuction returns (bool) {
         uint newBid = bids[msg.sender].add(msg.value);
+        require(newBid >= 10000, "bid should be at least 10000 Wei");
         require(newBid >  highestBid, "can/'t bid, Make a higher Bid");
 
         highestBidder = msg.sender;
@@ -71,7 +81,9 @@ contract Auction {
     }
 
     function  withdraw() public returns (bool) {
-        require(block.timestamp > auctionEnd , "can't withdraw, Auction is still  open");
+        require(_auctionEnded(), "can't withdraw, Auction is still  open");
+        require(highestBidder != msg.sender, "Highest Bidder is not allowed withdraw");
+
         uint amount = bids[msg.sender];
         bids[msg.sender] = 0;
 
